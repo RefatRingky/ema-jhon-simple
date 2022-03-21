37a1d6d4f01e9cf029e03678d5ed16b8bba306ad
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-22 02:11:29 +0600

    second commit

---
+++ src/component/Shop/Shop.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    const [products,setProduct] = useState([]);
+    useEffect(()=>{
+      fetch('products.json')
+      .then(res => res.json())
+      .then(data => setProduct(data))
+    },[])
+    return (
+        <div className='shop-container'>
+            <div className='products-container'>
+              <h1>This is for products:{products.length}</h1>
+            </div>
+            <div className='cart-container'>
+              <h1>This is for summary</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;

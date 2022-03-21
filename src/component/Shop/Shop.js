ca0bc49c71a1fc2536805437ba1d2cbc26d23afc
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-22 02:34:23 +0600

    third commit

--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Products/Product';
 import './Shop.css';
 
 const Shop = () => {
@@ -11,7 +12,11 @@ const Shop = () => {
     return (
         <div className='shop-container'>
             <div className='products-container'>
-              <h1>This is for products:{products.length}</h1>
+              {
+                  products.map(product =><Product key={product.id}
+                  product={product}
+                  ></Product>)
+              }
             </div>
             <div className='cart-container'>
               <h1>This is for summary</h1>

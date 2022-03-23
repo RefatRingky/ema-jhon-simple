073e1266a8ef00d5f3faa11ae903f0054db48b5d
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-24 01:48:22 +0600

    total price calculate

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({cart}) => {
+    let total = 0;
+    for(const product of cart){
+        total = total + product.price;
+    }
+
     return (
-        <div>
+        <div className='cart'>
             <h1>This is for summary</h1>
               <p>Selected Item:{cart.length}</p>
+              <p>Total Price:${total}</p>
+              <p>Total Shipping:</p>
+              <p>Tax:</p>
+              <h4>Grand Total</h4>
         </div>
     );
 };

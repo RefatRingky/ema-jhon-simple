ac93204c07db5145bd25d3e56679c215b002e2a7
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-31 00:27:26 +0600

    react router commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './component/Header/Header';
+import Inventory from './component/Inventory/Inventory';
+import Oders from './component/Oders/Oders';
 import Shop from './component/Shop/Shop';
 
 function App() {
   return (
     <div >
        <Header></Header>
-       <Shop></Shop>
+       <Routes>
+         <Route path='/' element={<Shop></Shop>}></Route>
+         <Route path='/shop' element={<Shop></Shop>}></Route>
+         <Route path='/orders' element={<Oders></Oders>}></Route>
+         <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+       </Routes>
+    
     </div>
   );
 }
